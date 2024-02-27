aa5621bc172315ffd87d3cd367826905bdafc91c
Igor Świerczyński <swierczynskigor@gmail.com>
2024-02-27 19:52:39 +0100

    navbar fix

--- src/components/reuseable/Navbar.tsx
+++ src/components/reuseable/Navbar.tsx
@@ -11,34 +11,37 @@ const Navbar = () => {
   const isVisible = useHideOnScrollDown();
 
   const props = useSpring({
-    to: { top: isVisible ? "0" : "-120px" },
+    to: { top: isVisible ? "0" : "-140px" },
     config: { ...config.wobbly, clamp: true },
   });
 
   return (
     <animated.header
-      className="w-full flex justify-between py-8 px-14 items-center z-30 fixed"
+      className="w-full flex justify-between py-8 px-14 items-center z-30 fixed h-26 bg-[#171717] overflow-hidden"
       style={props}
     >
-      <div className="flex items-center gap-20">
-        <Image src={logo} alt="logo" width={170} />
-        <Link href="/how" className="text-xl">
-          How it works
-        </Link>
-        <Link href="/lending" className="text-xl">
-          Lending
-        </Link>
-        <Link href="/insurance" className="text-xl">
-          Insurance
-        </Link>
-        <Link href="/buyaloan" className="text-xl">
-          Buy a loan
-        </Link>
-      </div>
-      <div>
-        <Button variant="default" size="lg">
-          Connect wallet
-        </Button>
+      <div className="h-[100vw] w-1/3 absolute top-[-100px] left-[-200px] xl:left-[-400px] blur-[200px] bg-[#25309D] z-1"></div>
+      <div className="relative w-full flex justify-between overflow-hidden">
+        <div className="flex items-center gap-20">
+          <Image src={logo} alt="logo" width={170} />
+          <Link href="/how" className="text-xl">
+            How it works
+          </Link>
+          <Link href="/lending" className="text-xl">
+            Lending
+          </Link>
+          <Link href="/insurance" className="text-xl">
+            Insurance
+          </Link>
+          <Link href="/buyaloan" className="text-xl">
+            Buy a loan
+          </Link>
+        </div>
+        <div>
+          <Button variant="default" size="lg">
+            Connect wallet
+          </Button>
+        </div>
       </div>
     </animated.header>
   );
