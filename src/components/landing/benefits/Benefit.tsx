7aaa82355d204f34f9a528163415a6f3cc7d9bd9
JanGospodarek <jan.gospo@onet.pl>
2024-03-13 11:28:15 +0100

    waves

--- src/components/landing/benefits/Benefit.tsx
+++ src/components/landing/benefits/Benefit.tsx
@@ -12,22 +12,24 @@ const Benefit = (props: Props) => {
   const { reverse, image, heading, description, btnName } = props;
   return (
     <div
-      className={`flex z-20 mx-0 md:mx-16 lg:mx-48 justify-center ${
+      className={`flex z-20 mx-0 md:mx-16 lg:mx-48 justify-center flex-col sm:flex-row ${
         reverse
           ? "flex-row-reverse lg:translate-x-[-100px]"
           : "flex-row lg:translate-x-[100px]"
       }`}
     >
-      <div className="text-center flex flex-col gap-2 items-center p-16">
+      <div className="text-center flex flex-col gap-2 items-center p-4 sm:p-16">
         <div className="text-4xl font-bold">{heading}</div>
-        <div className="  text-xl font-extralight">{description}</div>
+        <div className="  text-xl font-extralight min-w-[200px]">
+          {description}
+        </div>
         <Button variant="primary">{btnName}</Button>
       </div>
       <div className="flex justify-center items-center ">
         <Image
           src={image}
           alt="benefit"
-          className="md:w-[1000px] lg:w-[1500px] h-auto"
+          className="w-[400px] sm:w-[1000px] lg:w-[1500px] "
         />
       </div>
     </div>
--- src/components/landing/features/Features.tsx
+++ src/components/landing/features/Features.tsx
@@ -3,7 +3,7 @@ import Feature from "./Feature";
 import gear from "../../../../public/gears.png";
 const Feautres = () => {
   return (
-    <div className="mx-32 flex gap-32 justify-center py-24">
+    <div className=" mx-4 sm:mx-32 flex gap-10 sm:gap-32 justify-center p sm:py-24 flex-col sm:flex-row">
       <Feature
         heading="Stay up to date"
         description="You can allow notifications on your loan offers and lends to stay updated with your assets. "
